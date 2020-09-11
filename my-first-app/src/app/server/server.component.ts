cf8642afdef6e8b3cb1b7086c108993034e4c1ae
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-11 08:46:47 +0200

    Databinding

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  // tslint:disable-next-line: no-inferrable-types
+  serverId: number = 10;
+  serverStatus: string = 'offline';
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
