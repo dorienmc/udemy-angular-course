c6b3160643c31ee3f6fb04d90235d414b3f7b768
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-11 14:13:27 +0200

    Using the components

--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
