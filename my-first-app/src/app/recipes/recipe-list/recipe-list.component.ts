eb88790edb954e704a1c14a6be28027b93c6b263
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-15 15:28:13 +0200

    Passing data with event and property binding

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,9 +9,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('Chili Con Carne', 'Beans with meat', 'https://www.24kitchen.nl/files/styles/social_media_share/public/2019-10/shutterstock_1192492630.jpg?itok=RCqTgyci'),
-    new Recipe('Schnitzel', 'Flattened meat with breadcrums','https://favorflav.com/images/shutterstock_669627406.jpg')
+    new Recipe('Schnitzel', 'Flattened meat with breadcrums', 'https://favorflav.com/images/shutterstock_669627406.jpg')
   ];
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.selectedRecipe.emit(recipe);
+  }
 
 }
