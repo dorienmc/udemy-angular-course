7c472e37b8849487a0efae6cfdaf5fd029fa0bdd
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-15 14:21:05 +0200

    Navigation with Event Binding and ngIf

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+  selectedFeature = 'recipes';
+
+  onNavigate(feature: string): void {
+    console.log(feature);
+    this.selectedFeature = feature;
+  }
 }
