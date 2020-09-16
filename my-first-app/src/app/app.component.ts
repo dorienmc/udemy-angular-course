9cc14832402d3582efebd6dda8b4d4134bb839bf
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-16 09:33:46 +0200

    Move users from App to respective components

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { UsersService } from './users-service.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  activeUsers: string[];
-  inactiveUsers: string[];
-
-  constructor(private usersService: UsersService){}
-
-  ngOnInit() {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
-  }
-
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
+export class AppComponent {
+  constructor(){}
 }
