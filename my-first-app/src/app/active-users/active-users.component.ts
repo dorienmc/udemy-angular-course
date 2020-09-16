e063a9598e09ec85b15fcf81aad7fef0c6d9a54f
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-16 09:29:59 +0200

    Create and use the UsersService

---
+++ my-first-app/src/app/active-users/active-users.component.ts
@@ -0,0 +1,17 @@
+import { UsersService } from './../users-service.service';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-active-users',
+  templateUrl: './active-users.component.html',
+  styleUrls: ['./active-users.component.css']
+})
+export class ActiveUsersComponent {
+  @Input() users: string[];
+
+  constructor(private usersService: UsersService){}
+
+  onSetToInactive(id: number) {
+    this.usersService.setToInactive(id);
+  }
+}
