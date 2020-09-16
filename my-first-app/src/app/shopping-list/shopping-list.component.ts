70bfeb115b7bf0905526b6d375d3b216af01ec7e
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-16 11:59:01 +0200

    Adding the shopping list service

--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,21 +1,20 @@
 import { Ingredient } from './../shared/ingredient.model';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Beans', 1),
-    new Ingredient('Tomatos', 5)
-  ];
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) {
+  }
 
-  onIngredientAdded(ingredient: Ingredient): void{
-    this.ingredients.push(ingredient);
+  ngOnInit(): void {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
 }
--- my-first-app/src/app/shopping-list/shopping-list.service.ts
+++ my-first-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Beans', 1),
+    new Ingredient('Tomatos', 5),
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+  }
 }
