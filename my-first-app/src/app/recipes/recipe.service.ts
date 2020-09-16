9f7d33f3a833b20f8cfd919017d4d0ecb17a4210
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-16 14:56:29 +0200

    Adding ingredients to recipes

--- my-first-app/src/app/recipes/recipe.service.ts
+++ my-first-app/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -8,12 +9,23 @@ export class RecipeService {
     new Recipe(
       'Chili Con Carne',
       'Beans with meat',
-      'https://www.24kitchen.nl/files/styles/social_media_share/public/2019-10/shutterstock_1192492630.jpg?itok=RCqTgyci'
+      'https://www.24kitchen.nl/files/styles/social_media_share/public/2019-10/shutterstock_1192492630.jpg?itok=RCqTgyci',
+      [
+        new Ingredient('Beans (canned)', 1),
+        new Ingredient('Chili (tsp)', 1),
+        new Ingredient('Minced meat (gr)', 400),
+        new Ingredient('Tomatoes', 2)
+      ]
     ),
     new Recipe(
       'Schnitzel',
       'Flattened meat with breadcrums',
-      'https://favorflav.com/images/shutterstock_669627406.jpg'
+      'https://favorflav.com/images/shutterstock_669627406.jpg',
+      [
+        new Ingredient('Pork meat (gr)', 300),
+        new Ingredient('Breadcrumbs (gr)', 50),
+        new Ingredient('French fries', 30)
+      ]
     ),
   ];
 
