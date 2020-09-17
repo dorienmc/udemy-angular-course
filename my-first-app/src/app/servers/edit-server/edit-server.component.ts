c6467a5136e11a97b5aaa2f8687e911fb24fef28
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-17 17:41:23 +0200

    Using query parameters

--- my-first-app/src/app/servers/edit-server/edit-server.component.ts
+++ my-first-app/src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -12,12 +12,17 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
-    // Now you can retrieve/subscribe to queryParams and fragment
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => {
+        this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+      }
+    );
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
--- my-first-app/src/app/servers/server/server.component.ts
+++ my-first-app/src/app/servers/server/server.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -11,7 +11,10 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     const id: number = this.route.snapshot.params['id'];
@@ -22,6 +25,11 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     );
+  }
+
+  onEdit() {
+    // this.router.navigate(['/servers', this.server.id, 'edit']);
+    this.router.navigate( ['edit'], { relativeTo: this.route});
   }
 
 }
