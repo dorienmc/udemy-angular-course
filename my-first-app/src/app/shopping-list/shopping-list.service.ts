f31a0ec00925283ef24a08b2850c03dcb2c48519
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-18 15:03:58 +0200

    Improve the Reactive Service with Observables

--- my-first-app/src/app/shopping-list/shopping-list.service.ts
+++ my-first-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,11 +1,12 @@
-import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Beans', 1),
@@ -20,11 +21,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient): void {
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 
   addIngredients(ingredients: Ingredient[]): void {
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 }
