2589bac34736b6aa46339cd5c832849e1dd6782b
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-18 14:53:43 +0200

    Subject vs Emitter (part 2)

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,18 +1,23 @@
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   userActivated = false;
+  private activatedSub: Subscription;
 
   constructor(private userService: UserService) {}
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe( didActivate => {
+    this.activatedSub = this.userService.activatedEmitter.subscribe( didActivate => {
       this.userActivated = didActivate;
     })
   }
