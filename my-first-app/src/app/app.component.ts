0366686fbc5a316b4f789508380093076484db50
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-24 11:24:19 +0200

    Set default value for subscription

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
   email: string;
-  subscription: string;
+  subscription = 'advanced';
   password: string;
 
-  onSubmit(form: NgForm) {
-    this.email = form.value.email;
-    this.subscription = form.value.subscription;
-    this.password = form.value.password;
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.email = this.signupForm.value.email;
+    this.subscription = this.signupForm.value.subscription;
+    this.password = this.signupForm.value.password;
   }
 }
