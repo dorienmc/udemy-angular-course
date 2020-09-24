c44b346a29e638b3ad96f8efd670a85395d0759d
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-24 10:17:33 +0200

    Register the controls and submit the form

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  email: string;
+  subscription: string;
+  password: string;
 
+  onSubmit(form: NgForm) {
+    this.email = form.value.email;
+    this.subscription = form.value.subscription;
+    this.password = form.value.password;
+  }
 }
