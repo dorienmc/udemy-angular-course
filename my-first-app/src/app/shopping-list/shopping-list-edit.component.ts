bb5f1001365d4ee5a989c9c393f93c55c84a62fd
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-24 08:45:32 +0200

    Clear shopping list

--- my-first-app/src/app/shopping-list/shopping-list-edit.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list-edit.component.ts
@@ -5,21 +5,23 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class ShoppingListEditComponent implements OnInit{
+export class ShoppingListEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef;
   @ViewChild('amountInput') amountInputRef: ElementRef;
 
-  constructor(private shoppingListService: ShoppingListService){}
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onAddItem(): void {
     const name = this.nameInputRef.nativeElement.value;
     const amount = this.amountInputRef.nativeElement.value;
     this.shoppingListService.addIngredient(new Ingredient(name, amount));
+  }
+
+  onClear(): void {
+    this.shoppingListService.clear();
   }
 }
--- my-first-app/src/app/shopping-list/shopping-list.service.ts
+++ my-first-app/src/app/shopping-list/shopping-list.service.ts
@@ -28,4 +28,9 @@ export class ShoppingListService {
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.getIngredients());
   }
+
+  clear(): void {
+    this.ingredients = [];
+    this.ingredientsChanged.next(this.getIngredients());
+  }
 }
