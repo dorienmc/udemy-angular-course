3782e36f042f0167b77f4aadaf5ee498c50fb16d
Dorien Meijer Cluwen <dorien.meijercluwen@nedap.com>
2020-09-25 15:32:32 +0200

    Add simple validators

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.projectForm = new FormGroup({
-      'projectName': new FormControl(null),
-      'email': new FormControl(null),
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'projectStatus': new FormControl(null)
     });
   }
@@ -24,15 +24,6 @@ export class AppComponent implements OnInit {
 }
 
 // <!--
-//           Create a Form with the following Controls and Validators
-//           1) Project Name (should not be empty)
-//           2) Mail (should not be a empty and a valid email)
-//           3) Project Status Dropdown, with three values: 'Stable', 'Critical', 'Finished'
-//           4) Submit Button
-
 //           Add your own Validator which doesn't allow "Test" as a Project Name
-
 //           Also implement that Validator as an async Validator (replace the other one)
-
-//           Upon submitting the form, simply print the value to the console
 //       -->
