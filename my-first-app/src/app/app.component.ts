f6e94a97f27e4f7874d9a1ccbb48bb8bb7846907
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2020-12-22 21:27:06 +0100

    Handling errors

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,59 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import { Post } from './post.model';
+import {PostsService} from './posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts = [];
-  firebaseUrl = 'https://udemy-ng-database-default-rtdb.europe-west1.firebasedatabase.app/';
   isFetching = false;
+  error = null;
+  private errorSub: Subscription;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {
+  }
 
   ngOnInit(): void {
-    this.fetchPosts();
+    this.errorSub = this.postsService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });
   }
 
   onCreatePost(postData: { title: string; content: string }): void {
-    // Send Http request
-    this.http.post<{name: string}>(this.firebaseUrl + 'posts.json', postData)
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
-  onFetchPosts(): void{
-    // Send Http request
-    this.fetchPosts();
+  onFetchPosts(): void {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    }, error => {
+      this.error = error.message;
+    });
   }
 
   onClearPosts(): void {
     // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
 
-  private fetchPosts(): void {
-    this.isFetching = true;
-    this.http
-      .get<{[key: string]: Post}>(this.firebaseUrl + 'posts.json')
-      .pipe(
-        //map((responseData: {[key: string]: Post}) => {
-        map(responseData => {
-        const postsArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postsArray.push({...responseData[key]});
-          }
-        }
-        return postsArray;
-      }))
-      .subscribe(posts => {
-        this.isFetching = false;
-        this.loadedPosts = posts;
-    });
+  ngOnDestroy(): void {
+    this.errorSub.unsubscribe();
+  }
+
+  onHandleError(): void {
+    this.error = null;
+    this.isFetching = false;
   }
 }
---
+++ my-first-app/src/app/posts.service.ts
@@ -0,0 +1,54 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Post} from './post.model';
+import {catchError, map} from 'rxjs/operators';
+import {Observable, Subject, throwError } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class PostsService {
+  firebaseUrl = 'https://udemy-ng-database-default-rtdb.europe-west1.firebasedatabase.app/posts.json';
+  error = new Subject<string>();
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  createAndStorePost(title: string, content: string): void {
+    const postData: Post = {title, content};
+    // Send Http request
+    this.http
+      .post<{name: string}>(this.firebaseUrl, postData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      },
+        error => {
+        this.error = error.message;
+        });
+  }
+
+  fetchPosts(): Observable<Post[]> {
+    return this.http
+      .get<{[key: string]: Post}>(this.firebaseUrl)
+      .pipe(
+        map(responseData => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key]});
+            }
+          }
+          return postsArray;
+        }),
+        catchError(errorRes => {
+          // Send to analytics server
+          // ...
+          // Re throw the error
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts(): Observable<any> {
+    return this.http.delete(this.firebaseUrl);
+  }
+}
