181ff93b897352b322284a9e6a8f1b2515a8e6ea
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2020-12-23 11:27:47 +0100

    Storing recipes

--- my-first-app/src/app/shared/data-storage.service.ts
+++ my-first-app/src/app/shared/data-storage.service.ts
@@ -1,10 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
+  rootUrl = 'https://udemy-ng-database-default-rtdb.europe-west1.firebasedatabase.app/';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
 
+  }
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(this.rootUrl + 'recipes.json', recipes).subscribe( response => {
+        console.log(response);
+      }
+    );
   }
 }
