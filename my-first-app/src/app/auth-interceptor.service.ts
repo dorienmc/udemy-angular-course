8988ad61938455a95c1475f42372b569d57b3988
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2020-12-23 10:19:35 +0100

    Multiple interceptors

--- my-first-app/src/app/auth-interceptor.service.ts
+++ my-first-app/src/app/auth-interceptor.service.ts
@@ -4,19 +4,10 @@ import {tap} from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log(req);
-    console.log('Request is on its way');
     // Modify request (request is immutable, so we have to clone it)
-    const modifiedRequest = req.clone({headers: req.headers.append('Auth','xyz')});
+    const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
 
     // Continue request
-    return next.handle(modifiedRequest).pipe(tap(event => {
-      // Update response
-      console.log(event);
-      if (event.type === HttpEventType.Response) {
-        console.log('Response arrived, body data: ');
-        console.log(event.body);
-      }
-    }));
+    return next.handle(modifiedRequest);
   }
 }
