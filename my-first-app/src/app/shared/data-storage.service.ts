96a095e9f03bd0bb4bae955055cd4b63cc0bb6da
Dorien Lorijn <dorien.meijercluwen@nedap.com>
2020-12-23 11:57:05 +0100

    Transforming response data

--- my-first-app/src/app/shared/data-storage.service.ts
+++ my-first-app/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
@@ -20,8 +21,19 @@ export class DataStorageService {
   }
 
   fetchRecipes(): void {
-    this.http.get<Recipe[]>(this.rootUrl + 'recipes.json').subscribe(recipes => {
-      this.recipeService.setRecipes(recipes);
-    });
+    this.http.get<Recipe[]>(this.rootUrl + 'recipes.json')
+      .pipe(map( recipes => {
+        // Firebase doesn't store empty arrays, hence we have to put them bac
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }))
+      .subscribe(recipes => {
+        this.recipeService.setRecipes(recipes);
+      }
+    );
   }
 }
